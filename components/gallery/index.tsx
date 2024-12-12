1d7ad4d4bef6c63c2ee2023f581ee3ce8492f831
victorbragapontocloud <victorbraga@ponto.cloud>
2024-12-11 21:59:43 -0300

    fix: updated packages version and lightbox gallery

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import { Gallery, Item } from "react-photoswipe-gallery";
+import "photoswipe/dist/photoswipe.css";
 
-export default function Gallery({ id }: { id: string }) {
+export default function CustomGallery({ id }: { id: string }) {
   const images: string[] = [
     "/slide-0.jpg",
     "/slide-2.jpg",
@@ -12,74 +14,48 @@ export default function Gallery({ id }: { id: string }) {
     "/slide-6.jpg",
   ];
 
-  const [isLightboxOpen, setLightboxOpen] = useState<boolean>(false);
-  const [currentImage, setCurrentImage] = useState<string | null>(null);
-
-  const openLightbox = (image: string): void => {
-    setCurrentImage(image);
-    setLightboxOpen(true);
-  };
-
-  const closeLightbox = (): void => {
-    setLightboxOpen(false);
-    setCurrentImage(null);
-  };
-
   return (
     <div id={id}>
       {/* Gallery */}
-      <div className="grid grid-cols-3 ">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="relative group cursor-pointer overflow-hidden"
-            onClick={() => openLightbox(image)} // Abrir Lightbox ao clicar
-          >
-            <Image
-              src={image}
-              alt={`Gallery Image ${index + 1}`}
-              width={300}
-              height={200}
-              className="object-cover w-full h-auto"
-            />
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-80 group-hover:backdrop-blur-sm transition-all duration-300 flex items-center justify-center">
-              <Image
-                src="/logo-garota-rio.png" // Substitua pelo caminho da sua logo
-                alt="Logo"
-                width={160}
-                height={160}
-                className="opacity-70"
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Lightbox */}
-      {isLightboxOpen && currentImage && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
-          onClick={closeLightbox} // Fechar ao clicar fora da imagem
-        >
-          <div className="relative">
-            <Image
-              src={currentImage}
-              alt="Lightbox Image"
-              width={800}
-              height={500}
-              className="object-contain max-w-full max-h-full"
-              priority // Priorizar o carregamento da imagem
-            />
-            <button
-              onClick={closeLightbox}
-              className="absolute top-2 right-2 text-white text-xl bg-black bg-opacity-50 p-2 rounded-full"
+      <Gallery>
+        <div className="grid grid-cols-3">
+          {images.map((image, index) => (
+            <Item
+              key={index}
+              original={image}
+              thumbnail={image}
+              width="800"
+              height="600"
             >
-              ✕
-            </button>
-          </div>
+              {({ ref, open }) => (
+                <div
+                  ref={ref}
+                  className="relative group cursor-pointer overflow-hidden"
+                  onClick={open} // Abrir Lightbox ao clicar
+                >
+                  <Image
+                    src={image}
+                    alt={`Gallery Image ${index + 1}`}
+                    width={300}
+                    height={200}
+                    className="object-cover w-full h-auto"
+                  />
+                  {/* Overlay */}
+                  <div className="absolute inset-0 bg-black bg-opacity-80 group-hover:backdrop-blur-sm transition-all duration-300 flex items-center justify-center">
+                    <Image
+                      src="/logo-garota-rio.png" // Substitua pelo caminho da sua logo
+                      alt="Logo"
+                      width={160}
+                      height={160}
+                      className="opacity-70"
+                    />
+                  </div>
+                </div>
+              )}
+            </Item>
+          ))}
         </div>
-      )}
+      </Gallery>
     </div>
   );
 }
---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
