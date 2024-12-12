cb92e0cdedf690f644f3a90e713e575830d10761
victorbragapontocloud <victorbraga@ponto.cloud>
2024-12-12 12:52:06 -0300

    fix: ensure priority loading

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -5,14 +5,15 @@ export default function CarouselImg() {
   const images = [{ src: "/slide-1.jpg", width: 1920, height: 1080 }];
 
   return (
-    <div className="relative w-screen h-[80vh]">
+    <div className="relative w-screen h-[80vh] bg-[#f4e2d8]">
       <div className="absolute inset-0 flex items-center justify-center z-10">
         <Image
           src="/logo.png"
           alt="Garota Rio Bronze Logo"
           width={600}
           height={600}
-          className="object-contain p-3"
+          className="object-contain"
+          priority
         />
       </div>
       <div className="flex w-full h-full">
@@ -27,6 +28,7 @@ export default function CarouselImg() {
                     layout="fill"
                     objectFit="cover"
                     className="responsive-image"
+                    priority
                   />
                 </div>
               </div>
